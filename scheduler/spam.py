d415ee3d9834343f0844b69c055c32fe8e751cac
spac3orange <m4gi3k44@proton.me>
2024-01-09 17:28:47 +0600

    upd 4.6 spb check added

--- scheduler/spam.py
+++ scheduler/spam.py
@@ -1,5 +1,5 @@
 import random
-
+from config import logger
 from config import TelethonConnect
 from database import _tasks
 from scheduler import main_scheduler
@@ -8,14 +8,23 @@ import asyncio
 
 
 async def process_spam(accounts, user_message, timing, task_id):
-    accounts = accounts.split(',')
-    accounts = [x.strip() for x in accounts]
-    await inform_admins(f'Начинаю выполнение задачи {task_id}...')
-    for acc in accounts:
-        session = TelethonConnect(acc)
-        task = asyncio.create_task(session.spam_groups(user_message, timing))
-        await asyncio.sleep(random.randint(0, 10))
-    await asyncio.sleep(60)
-    await _tasks.db_execute_scheduled_task(int(task_id))
-    await inform_admins(f'Задача {task_id} выполнена.')
+    try:
+        accounts = accounts.split(',')
+        accounts = [x.strip() for x in accounts]
+        await inform_admins(f'Начинаю выполнение задачи {task_id}...')
+        for acc in accounts:
+            try:
+                session = TelethonConnect(acc)
+                task = asyncio.create_task(session.spam_groups(user_message, timing))
+                await asyncio.sleep(random.randint(0, 10))
+            except Exception as e:
+                logger.error(e)
+                pass
+    except Exception as e:
+        logger.error(e)
+        pass
+    finally:
+        await asyncio.sleep(60)
+        await _tasks.db_execute_scheduled_task(int(task_id))
+        await inform_admins(f'Задача {task_id} выполнена.')
 
